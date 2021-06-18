a0ff6539c895a17c70cdacf7ea7923fbced5caba
Ankush Kumar <ankushkashyap0000@gmail.com>
2021-06-18 16:10:55 +0530

    query and mutation cleanup done

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
-import re
-from flask import Flask, render_template, request, json, make_response, jsonify
+from flask import Flask, request, json, make_response, jsonify
 from flask_cors import CORS
-from flask_jwt import jwt
-from bson import ObjectId
-from functools import wraps
-from graphene.types import mutation
+from graphene.types.objecttype import ObjectType
 from mongoengine import connect
 from flask_graphql import GraphQLView
-
 from src.user.api.query import UserQuery
-from src.user.api.mutation import MyMutations
+from src.user.api.mutation import UserMutations
 from src.user.data.model import User
-
 from graphene_federation import build_schema
 
 
@@ -24,9 +18,15 @@ app.config['SECRET_KEY'] = "randomsecretkey"
 
 connect(host="mongodb://127.0.0.1:27017/user_service_database")
 
+class Query(UserQuery , ObjectType):
+    pass
+
+class Mutation(UserMutations, ObjectType):
+    pass
 
 
-schema = build_schema(query=UserQuery, mutation=MyMutations)
+
+schema = build_schema(query=Query, mutation=Mutation)
 # print(schema)
 
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view(
@@ -43,26 +43,23 @@ def hello_world():
     return 'Hello, World!'
 
 
-#signup route
-@app.route('/auth/signup', methods=["POST"])
-def Signup():
+# #signup route
+# @app.route('/auth/signup', methods=["POST"])
+# def Signup():
      
-    if request.method == "POST":
+#     if request.method == "POST":
         
-        request_data = json.loads(request.data)
-        
+#         request_data = json.loads(request.data)
+#         user = User(
+#             first_name=request_data["firstName"], 
+#             last_name =request_data["lastName"], 
+#             email =request_data["email"],
+#             password = request_data["password"]
+#              )
+#         print(user)
+#         user.save()
 
-         
-        user = User(
-            first_name=request_data["firstName"], 
-            last_name =request_data["lastName"], 
-            email =request_data["email"],
-            password = request_data["password"]
-             )
-        print(user)
-        user.save()
-
-        return make_response('Successfully registered.', 201)
+#         return make_response('Successfully registered.', 201)
 
  
 if __name__ == "__main__":
--- src/user/api/schema.py
+++ src/user/api/schema.py
@@ -1,10 +1,5 @@
-from graphene import ObjectType, String, Schema, Field,List, ID
-from graphene_federation import  key, provides
+from graphene import ObjectType, String
 
-#GraphQL Schema
-
-
-@provides
 class User(ObjectType):
     first_name = String()
     last_name = String()
